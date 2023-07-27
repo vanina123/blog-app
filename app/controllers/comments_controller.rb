1979405287ed7479091fb5389940af8ea90222e4
Nana Vanina <vaninalarissa@gmail.com>
2023-07-27 13:14:05 +0100

    blog app views

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,17 @@
+# def create
+#     @comment = Comment.new(comment_params)
+#     @comment.save
+#     redirect_to comments_path
+#   end
+
+#   def destroy
+#     @comment = Comment.find(params[:id])
+#     @comment.destroy
+#     redirect_to comments_path
+#   end
+
+#   private
+
+#   def comment_params
+#     params.require(:comment).permit(:author_id, :post_id)
+#   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  resources :user, only: [:index, :show] do 
-    resources :post, only: [:index, :show] do
+  root 'users#index'
+
+  resources :users, only: [:index, :show, :new, :create] do
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :likes, only: [:create]
+      resources :comments, only: [:new, :create]
     end
   end
 end
