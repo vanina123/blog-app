1729fa66ecf5558d375bd9c6add828e39696ef0c
Nana Vanina <vaninalarissa@gmail.com>
2023-08-05 20:42:42 +0100

    create api endpoints

--- config/application.rb
+++ config/application.rb
@@ -13,8 +13,14 @@ module BlogApp
 
     # Configuration for the application, engines, and railties goes here.
     #
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+    config.middleware.use ActionDispatch::Flash
+
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
+    config.api_only = true
+    config.debug_exception_response_format = :api
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,17 @@ Rails.application.routes.draw do
       resources :comments, only: [:new, :create]
     end
   end
+
+  namespace :api do
+    namespace :v1 do
+      get 'comments/index'
+      get 'comments/create'
+      get 'posts/index'
+      resources :users, only: [] do
+        resources :posts, only: [:index] do
+          resources :comments, only: %i[index create]
+        end
+      end
+    end
+  end
 end
